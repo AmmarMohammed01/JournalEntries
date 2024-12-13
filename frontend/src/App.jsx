4e1f9784062a0c4e9f463bf29729c6285e5b4506
Ammar Mohammed <ammar.hm321@gmail.com>
2024-12-12 18:38:25 -0600

    Basic form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,38 @@
 import { useState } from 'react'
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [entry, setEntry] = useState("");
+
+  function handleSubmit(e) {
+    // Prevent the browser from reloading the page
+    e.preventDefault();
+
+    // Read the form data
+    const form = e.target;
+    const formData = new FormData(form);
+
+    // You can work with it as a plain object:
+    const formJson = Object.fromEntries(formData.entries());
+    console.log(formJson);
+
+    setTitle(formJson.jTitle);
+    setEntry(formJson.jEntry);
+  }
 
   return (
     <>
       <p>Journal Entries</p>
 
-      <form action="">
-        <input type="text" placeholder='Enter title here...'/>
-        <input type="text" placeholder='Type to start journal entry...' />
-        <input type="submit" />
+      <form action="post" onSubmit={handleSubmit}>
+        <input type="text" name="jTitle" placeholder="Title here" />
+        <input type="text" name="jEntry" placeholder="Type entry here" />
+        <button type="reset">Reset form</button>
+        <button type="submit">Submit form</button>
       </form>
+
+      <p>{title}</p>
+      <p>{entry}</p>
     </>
   )
 }
